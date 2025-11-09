065e74642d10f12459ca8e0bb4cb541f6b7bdd9f
iemnothin <okysaputraa10@gmail.com>
2025-11-09 17:28:44 +0700

    update: db.py for production

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,21 @@
+# Production
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="",
-        database="db_airq"
+        user="abiila_admin",
+        password="2bGBTWV7@y#bnPH",
+        database="abiila_airq_db"
     )
+
+# Local
+# import mysql.connector
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="",
+#         database="db_airq"
+#     )
