8730f206a688de50303e25feb07d381b2b3ffd95
iemnothin <okysaputraa10@gmail.com>
2025-11-10 00:12:33 +0700

    fixing db connection

--- backend/routes.py
+++ backend/routes.py
@@ -579,7 +579,7 @@ def system_status():
     try:
         # 🔍 Ubah sesuai database kamu
         engine = create_engine(
-            "mysql+pymysql://abiila_admin:2bGBTWV7@y#bnPH@127.0.0.1/abiila_airq_db"
+            "mysql+pymysql://abiila_admin:2bGBTWV7%40y%23bnPH@127.0.0.1/abiila_airq_db"
         )
 
         with engine.connect() as conn:
